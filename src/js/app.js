de71712798a3f046110400e5afaaefd5fb92499d
Nick Cresner <nickcresner@gmail.com>
2017-05-19 14:02:21 +0100

    started to work on prototyping and game logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log('JS Loaded');
+$(() => {
+  console.log('JS Loaded');
+
+
+  const $map = $('div');
+  let userClickX = 0;
+  let userClickY = 0;
+
+  const $o2Centre = $('#o2');
+  const o2CentreObj = {xLocation: 817, yLocation: 510};
+
+  console.log(o2CentreObj.xLocation);
+  console.log(o2CentreObj.yLocation);
+
+  $map.on('click', (e) => {
+    console.log('clicked on div');
+    console.log(e.pageX);
+    console.log(e.pageY);
+    userClickX = e.pageX;
+    userClickY = e.pageY;
+
+    if (userClickX === o2CentreObj.xLocation && userClickY === o2CentreObj.yLocation) {
+      console.log('bullseye!');
+    }
+  });
+
+});
